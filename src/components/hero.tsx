f3383cd9a7e7188bab9f691a961fd3616400f4c1
Isai Sanzy <isai10sanchez@gmail.com>
2025-02-28 16:10:07 -0700

    added grainy texture to hero and made changes to responsiveness

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,27 +1,21 @@
 export default function Hero() {
   return (
-    <section
-      id="home"
-      className="pt-8 mt-12 md:mt-8 min-h-screen flex flex-col md:flex-row items-start relative bg-midnight-blue"
-    >
-      {/* Background image container with navy outline */}
+    <section className="pt-8 md:pt-18 lg:pt-24 min-h-[80vh] flex flex-col md:flex-row items-start relative bg-midnight-blue">
       <div
-        className="absolute inset-0 m-4 md:m-8 rounded-2xl overflow-hidden opacity-5 border-2 border-white rounded-xl"
+        className="absolute inset-0 opacity-50"
         style={{
-          backgroundImage: "url('/4048090.jpg')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
+          backgroundImage: "url('/grainy-texture.png')",
         }}
       ></div>
 
-      <div className="w-full md:w-1/2 p-8 md:p-16 flex flex-col justify-center relative z-10">
-        <div className="relative">
-          <h1 className="text-5xl md:text-4xl lg:text-5xl font-bold leading-tight mb-4 relative">
-            <span className="block">MODERN</span>
-            <span className="block text-desert-sun">WEB DESIGN</span>
-            <span className="block">FOR BUSINESSES</span>
-          </h1>
-        </div>
+      <div className="w-full md:w-1/2 p-8 xl:ml-12 flex flex-col justify-start sm:justify-center relative z-10">
+        <h1 className="text-4xl md:text-5xl 2xl:text-6xl xl:pt-8 font-bold leading-tight mb-4">
+          <span className="block">
+            MODERN <span className="hidden xl:inline">AND SLEEK</span>
+          </span>
+          <span className="block text-desert-sun">WEB DESIGN</span>
+          <span className="block">FOR BUSINESSES</span>
+        </h1>
         <p className="text-xl mb-6 max-w-md text-gray-200 bg-navy-dark/70 py-3 rounded-lg">
           Let us handle your online presence while you focus on what's
           important,{" "}
@@ -35,13 +29,6 @@ export default function Hero() {
           >
             Start A Project
           </a>
-
-          <a
-            href="#work"
-            className="flex items-center px-5 py-1.5 bg-transparent text-white rounded-md hover:bg-[#1E3A5F] transition-colors text-lg font-medium border border-[#1E3A5F]"
-          >
-            See Our Work
-          </a>
         </div>
       </div>
       <div className="w-full md:w-1/2 p-8 md:p-16 relative">
@@ -52,11 +39,8 @@ export default function Hero() {
             <img
               src="/5999404.png"
               alt="Creative design process"
-              className="w-full h-auto rounded-md"
+              className="w-full h-auto rounded-md z-10"
             />
-            <div className="absolute -top-4 -right-4 bg-white/10 backdrop-blur-md px-4 py-2 rounded-md border border-white transform rotate-3">
-              <span className="text-white font-bold text-lg">INNOVATE</span>
-            </div>
           </div>
         </div>
       </div>
